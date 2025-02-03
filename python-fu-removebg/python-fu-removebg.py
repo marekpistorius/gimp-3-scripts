a0ddddff6a56d3edd15f25aa32f23acbeba0e3c6
Marek Pistorius <me@maniu.eu>
2025-02-03 17:42:38 +0100

    Update python-fu-removebg.py
    Add Copyright Notice

--- python-fu-removebg/python-fu-removebg.py
+++ python-fu-removebg/python-fu-removebg.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# (C) 2025 Marek Pistorius
+# This file is part of gimp-python-fu-removebg .
+# gimp-python-fu-removebg is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+# gimp-python-fu-removebg is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Xfce-nameday-plugin. If not, see <https://www.gnu.org/licenses/>.
 
 import gi
 gi.require_version('Gimp', '3.0')
